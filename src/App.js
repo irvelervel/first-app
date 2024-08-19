646f5b3b1a165203dc63079fc8a49866c64f80e2
Stefano Casasola <ste.casasola@gmail.com>
2024-08-19 11:55:24 +0200

    adds some changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+
+// un componente React può semplicemente essere una funzione che ritorna
+// una sintassi simile all'HTML -> questa sintassi si chiama JSX
+// differenze tra HTML e JSX:
+// - tutti gli attributi "class" diventano "className"
+// - le variabili si iniettano nella sintassi più facilmente, con { }
+
+const myName = 'Stefano'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>PRIMO COMPONENTE REACT!</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. {myName}
         </p>
         <a
           className="App-link"
@@ -15,11 +24,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          EPICODE!
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App' // qui è come se importassimo './App.js'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// la riga 6 crea sulla base di un div vuoto (con id di "root") un "ELEMENTO RADICE"
+// la riga 7 utilizza questa radice per montare nella pagina il componente
+// React che si chiama App
